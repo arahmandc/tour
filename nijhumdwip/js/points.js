98416926ce7bbf67abbb97113e2e348d2a731f93
Atikur Rahman <arahman.dcc2@gmail.com>
2021-10-12 17:38:55 +0600

    up

---
+++ nijhumdwip/js/points.js
@@ -0,0 +1,51 @@
+var points = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Dhaka Medicel",
+        "image": "dm.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.397354512069995,23.727274486220001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Eden College",
+        "image": "eden.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.386033579040003,23.72871032254
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Start from Khilji Road",
+        "image": "start.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [90.365056022670004,23.772425483479999]
+      }
+    }
+  ]
+}
