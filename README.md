912acef67f2e442094391700e2ede595827ef02b
Atikur Rahman <arahman.dcc2@gmail.com>
2021-11-02 02:27:42 +0600

    comillah

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # tour
 
 1. [Nijhum Dwip Tour](https://arahmandc.github.io/tour/nijhumdwip)
+2. [Comilla Tour](https://arahmandc.github.io/tour/comilla)
---
+++ comilla/js/points.js
@@ -0,0 +1,325 @@
+var points = {
+  "type": "FeatureCollection",
+  "features": [
+      {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Deer Watching Place",
+        "image": "deer1.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          91.013149468750001,22.041861565480001
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Deer Watching Place",
+        "image": "deer2.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.982407328779999,22.04466469126
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Potato Chips Eating",
+        "image": "deer3.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.980787275880004,22.042667081009998
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Crab Curry",
+        "image": "crab.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.980965642780006,22.04069181949
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Brackish Swamp Forests",
+        "image": "forest.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          91.010913843509996,22.03939900025
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Char",
+        "image": "boat1.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          91.050310049060002,22.3218921104
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Special Showering",
+        "image": "bath.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.860452566150002,22.60149177537
+        ]
+      }
+    },
+///////
+
+  {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Buffalo Curd",
+        "image": "curd.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.98093882069,22.039994442409998
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Namar Bazar Sea Beach",
+        "image": "beach.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.973405795100007,22.03918522088
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Hilsa Fishing Trawler",
+        "image": "hilsha.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          91.013122646659994,22.03999568551
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Nijhum Dwip Ghat",
+        "image": "ghat1.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          91.032838224019997,22.082647454060002
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Local Engine Boat, Mukter Ghat",
+        "image": "boat.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          91.037070729730004,22.090272624200001
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Hungrynaki Breakfast",
+        "image": "breckfast.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          91.071252820189997,22.229374063009999
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Tomoroddi Ferry Ghat",
+        "image": "ridoy.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          91.075426337419998,22.291153817840001
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Sleeping",
+        "image": "sleeping.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.633086814880002,22.857827743120001
+        ]
+      }
+    },
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Eatting Pao with Froto ",
+        "image": "frutopao.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.586137728690005,23.531294210390001
+        ]
+      }
+    },
+
+
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Dhaka Medicel",
+        "image": "dm.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.397354512069995,23.727274486220001
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Eden College",
+        "image": "eden.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          90.386033579040003,23.72871032254
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ed1b2e",
+        "marker-size": "s",
+        "marker-symbol": "camera",
+        "place": "Start from Khilji Road",
+        "image": "start.jpg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [90.365056022670004,23.772425483479999]
+      }
+    }
+  ]
+}
